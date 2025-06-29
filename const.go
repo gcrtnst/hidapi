424a2c0eb6f36b4ad640614ed213660331b97358
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2025-06-30 01:36:23 +0900

    Add const.go

---
+++ const.go
@@ -0,0 +1,13 @@
+package hidapi
+
+// #include <hidapi/hidapi.h>
+import "C"
+
+const VersionMajor = C.HID_API_VERSION_MAJOR
+const VersionMinor = C.HID_API_VERSION_MINOR
+const VersionPatch = C.HID_API_VERSION_PATCH
+const Version = C.HID_API_VERSION
+const VersionString = C.HID_API_VERSION_STR
+const MaxReportDescriptorSize = C.HID_API_MAX_REPORT_DESCRIPTOR_SIZE
+
+func MakeVersion(mj, mn, p int) int { return (((mj) << 24) | ((mn) << 8) | (p)) }
