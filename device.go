1aab2ba2e952538fcdd3942fcb752e84dccab74e
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2025-07-01 01:31:58 +0900

    Add hidapi.Error

---
+++ device.go
@@ -0,0 +1,27 @@
+package hidapi
+
+// #include <hidapi/hidapi.h>
+import "C"
+import (
+	"runtime"
+	"sync"
+)
+
+type Device struct {
+	mu  sync.Mutex
+	cln runtime.Cleanup
+	dev *C.hid_device
+}
+
+func (d *Device) Close() error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if d.dev != nil {
+		C.hid_close(d.dev)
+	}
+	d.dev = nil
+
+	d.cln.Stop()
+	return nil
+}
---
+++ error.go
@@ -0,0 +1,72 @@
+package hidapi
+
+// #include <hidapi/hidapi.h>
+import "C"
+import (
+	"strconv"
+	"unicode/utf16"
+	"unsafe"
+)
+
+type Error struct {
+	Text string
+}
+
+func (err *Error) Error() string {
+	return err.Text
+}
+
+func hidError(dev *Device) error {
+	cdev := (*C.hid_device)(nil)
+	if dev != nil {
+		dev.mu.Lock()
+		defer dev.mu.Unlock()
+		cdev = dev.dev
+	}
+
+	cstr := C.hid_error(cdev)
+	gstr := wcharPtrToString(cstr)
+
+	text := gstr
+	if text == "hid_error is not implemented yet" {
+		text = "an error occurred with hidapi"
+	}
+
+	return &Error{Text: text}
+}
+
+func wcharPtrToString(p *C.wchar_t) string {
+	if C.sizeof_wchar_t == unsafe.Sizeof(uint16(0)) { // UTF-16
+		l := wcharPtrToStringLen(p)
+		u := (*uint16)(unsafe.Pointer(p))
+		s := unsafe.Slice(u, l)
+		d := utf16.Decode(s)
+		return string(d)
+	}
+
+	if C.sizeof_wchar_t == unsafe.Sizeof(rune(0)) { // UTF-32
+		l := wcharPtrToStringLen(p)
+		r := (*rune)(unsafe.Pointer(p))
+		s := unsafe.Slice(r, l)
+		return string(s)
+	}
+
+	panic("unexpected size of wchar_t: " + strconv.Itoa(C.sizeof_wchar_t))
+}
+
+func wcharPtrToStringLen(p *C.wchar_t) int {
+	l := 0
+	for {
+		var u unsafe.Pointer
+		var c C.wchar_t
+		u = unsafe.Pointer(p)
+		u = unsafe.Pointer(uintptr(u) + uintptr(l*C.sizeof_wchar_t))
+		c = *(*C.wchar_t)(u)
+
+		if c == 0 {
+			break
+		}
+		l++
+	}
+	return l
+}
