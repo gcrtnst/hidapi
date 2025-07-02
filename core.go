3e0abdcdee0a04ebdf822cc0bc8acb2bce334199
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2025-07-03 01:23:42 +0900

    Merge error.go into core.go

--- core.go
+++ core.go
@@ -2,12 +2,25 @@ package hidapi
 
 // #include <hidapi/hidapi.h>
 import "C"
-import "sync"
+import (
+	"strconv"
+	"sync"
+	"unicode/utf16"
+	"unsafe"
+)
 
 var (
 	hidMutex sync.Mutex
 	hidCount = 0
 )
+
+type Error struct {
+	Text string
+}
+
+func (err *Error) Error() string {
+	return err.Text
+}
 
 func hidEnter() error {
 	hidMutex.Lock()
@@ -50,4 +63,59 @@ func hidInit() error {
 func hidExit() bool {
 	ret := C.hid_exit()
 	return ret == 0
+}
+
+func hidError(dev *Device) error {
+	cdev := (*C.hid_device)(nil)
+	if dev != nil {
+		dev.mu.Lock()
+		defer dev.mu.Unlock()
+		cdev = dev.dev
+	}
+
+	cstr := C.hid_error(cdev)
+	gstr := convertWCharPtrToString(cstr)
+
+	text := gstr
+	if text == "hid_error is not implemented yet" {
+		text = "an error occurred with hidapi"
+	}
+
+	return &Error{Text: text}
+}
+
+func convertWCharPtrToString(p *C.wchar_t) string {
+	if C.sizeof_wchar_t == unsafe.Sizeof(uint16(0)) { // UTF-16
+		l := convertWCharPtrToStringLen(p)
+		u := (*uint16)(unsafe.Pointer(p))
+		s := unsafe.Slice(u, l)
+		d := utf16.Decode(s)
+		return string(d)
+	}
+
+	if C.sizeof_wchar_t == unsafe.Sizeof(rune(0)) { // UTF-32
+		l := convertWCharPtrToStringLen(p)
+		r := (*rune)(unsafe.Pointer(p))
+		s := unsafe.Slice(r, l)
+		return string(s)
+	}
+
+	panic("unexpected size of wchar_t: " + strconv.Itoa(C.sizeof_wchar_t))
+}
+
+func convertWCharPtrToStringLen(p *C.wchar_t) int {
+	l := 0
+	for {
+		var u unsafe.Pointer
+		var c C.wchar_t
+		u = unsafe.Pointer(p)
+		u = unsafe.Pointer(uintptr(u) + uintptr(l*C.sizeof_wchar_t))
+		c = *(*C.wchar_t)(u)
+
+		if c == 0 {
+			break
+		}
+		l++
+	}
+	return l
 }
