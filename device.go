344a78cfa26cf69fa2463f32ce896ef47c87ab7e
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2025-07-05 23:43:53 +0900

    Add hidapi.hidRef to hidapi.Device

--- device.go
+++ device.go
@@ -10,18 +10,32 @@ import (
 type Device struct {
 	mu  sync.Mutex
 	cln runtime.Cleanup
-	dev *C.hid_device
+	dev *hidDevice
 }
 
 func (d *Device) Close() error {
 	d.mu.Lock()
 	defer d.mu.Unlock()
 
+	var err error
+	if d.dev != nil {
+		err = d.dev.Close()
+	}
+
+	d.cln.Stop()
+	return err
+}
+
+type hidDevice struct {
+	ref hidRef
+	dev *C.hid_device
+}
+
+func (d *hidDevice) Close() error {
 	if d.dev != nil {
 		C.hid_close(d.dev)
 		d.dev = nil
 	}
 
-	d.cln.Stop()
-	return nil
+	return d.ref.Close()
 }
