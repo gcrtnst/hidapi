ea233096a55a3cfd70904b1b87151f3ed4bf4deb
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2025-07-06 23:07:03 +0900

    Add runtime versions

--- version.go
+++ version.go
@@ -14,3 +14,47 @@ const (
 func MakeVersion(major, minor, patch int) int {
 	return (((major) << 24) | ((minor) << 8) | (patch))
 }
+
+func RuntimeVersionMajor() int {
+	v := C.hid_version()
+	if v == nil {
+		return 0
+	}
+
+	return int(v.major)
+}
+
+func RuntimeVersionMinor() int {
+	v := C.hid_version()
+	if v == nil {
+		return 0
+	}
+
+	return int(v.minor)
+}
+
+func RuntimeVersionPatch() int {
+	v := C.hid_version()
+	if v == nil {
+		return 0
+	}
+
+	return int(v.patch)
+}
+
+func RuntimeVersion() int {
+	v := C.hid_version()
+	if v == nil {
+		return 0
+	}
+
+	major := int(v.major)
+	minor := int(v.minor)
+	patch := int(v.patch)
+	return MakeVersion(major, minor, patch)
+}
+
+func RuntimeVersionString() string {
+	c := C.hid_version_str()
+	return C.GoString(c)
+}
