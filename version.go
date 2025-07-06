621817cb82b42b2afcef451e0bfdfa98dea55183
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2025-07-06 23:06:40 +0900

    Prefix version constants with CompileTime

--- version.go
+++ version.go
@@ -3,11 +3,13 @@ package hidapi
 // #include <hidapi/hidapi.h>
 import "C"
 
-const VersionMajor = C.HID_API_VERSION_MAJOR
-const VersionMinor = C.HID_API_VERSION_MINOR
-const VersionPatch = C.HID_API_VERSION_PATCH
-const Version = C.HID_API_VERSION
-const VersionString = C.HID_API_VERSION_STR
+const (
+	CompileTimeVersionMajor  = C.HID_API_VERSION_MAJOR
+	CompileTimeVersionMinor  = C.HID_API_VERSION_MINOR
+	CompileTimeVersionPatch  = C.HID_API_VERSION_PATCH
+	CompileTimeVersion       = C.HID_API_VERSION
+	CompileTimeVersionString = C.HID_API_VERSION_STR
+)
 
 func MakeVersion(major, minor, patch int) int {
 	return (((major) << 24) | ((minor) << 8) | (patch))
