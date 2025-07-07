c3f0becca83a2be7ad96c83762eea544065cfdb5
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2025-07-08 00:49:47 +0900

    Remove individual runtime version functions

--- version.go
+++ version.go
@@ -15,33 +15,6 @@ func EncodeVersion(major, minor, patch int) int {
 	return (((major) << 24) | ((minor) << 8) | (patch))
 }
 
-func RuntimeVersionMajor() int {
-	v := C.hid_version()
-	if v == nil {
-		return 0
-	}
-
-	return int(v.major)
-}
-
-func RuntimeVersionMinor() int {
-	v := C.hid_version()
-	if v == nil {
-		return 0
-	}
-
-	return int(v.minor)
-}
-
-func RuntimeVersionPatch() int {
-	v := C.hid_version()
-	if v == nil {
-		return 0
-	}
-
-	return int(v.patch)
-}
-
 func RuntimeVersion() (int, int, int) {
 	v := C.hid_version()
 	if v == nil {
